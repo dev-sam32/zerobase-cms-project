5a7ab083d994f1e195be46b66a091ce216ef8154
dev-sam32 <shinhs91@gmail.com>
2024-05-19 23:09:32 +0900

    [init] 프로젝트 초기 설정 - Spring Boot - .gitignore - pull_request_template
    -

---
+++ src/test/java/com/zerobase/commerce/ZerobaseCmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerobaseCmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
